2e3f04131eb3f652e9c2e2b0a4fdbafcb876b48e
Anthony Calo <acalo4554@gmail.com>
2023-04-15 21:22:50 -0400

    First commit. Working flask server best move '/' post request

---
+++ app.py
@@ -0,0 +1,45 @@
+import sys
+import json
+from flask import Flask, render_template, jsonify, request, send_from_directory
+from flask_cors import CORS
+from flask_restx import Api, Resource, fields, reqparse
+import os
+from argparse import ArgumentParser
+from stockfish import Stockfish
+
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app, title="Calo Chess Stockfish API", description="Calo Chess stockfish api")
+ns = api.namespace("/", description="Calo Chess Stockfish")
+
+recordModel = api.model("recordModel",
+                        {
+    "content": fields.String(readonly=True, description="record Content")
+                        })
+
+def cmdArgParser():
+    parser= ArgumentParser(description="API Server chess Calo")
+    flask_group = parser.add_argument_group("Rest Server Options")
+    flask_group.add_argument("--port", "-p", type=int, help="Port to run server", required=True)
+    flask_group.add_argument("--debug", "-d", help="debug mode")
+    return parser
+
+@ns.route("/")
+class getMove(Resource):
+    def post(self):
+        data = request.json
+        #path to stockfish executable file
+        stockfish = Stockfish(path=r".\venv\Lib\site-packages\stockfish\stockfish_20011801_x64.exe")
+        stockfish.set_position(data["moves"])
+        bestmove = (stockfish.get_best_move())
+        return jsonify(bestmove)
+
+
+
+
+if __name__ == "__main__":
+    global args
+    args = cmdArgParser().parse_args()
+    app.run(host="0.0.0.0", port=args.port,debug=args.debug)
